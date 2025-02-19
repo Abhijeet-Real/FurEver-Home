c1395d4dacec7709e31ca978715ddd924b6851be
Abhijeet-Real <abhijeet1472@gmail.com>
2025-02-19 10:40:24 +0530

    Add index_sql.py to list SQL files for search engine indexing

---
+++ index_sql.py
@@ -0,0 +1,11 @@
+# Description: This file contains the list of SQL files that are to be indexed by the search engine.
+sql_files = [
+    "AA02RJ57 1 Database Schema Definition Script.sql",
+    "AA02RJ57 2 Database Automation & Integrity.sql",
+    "AA02RJ57 3 Initial Data Insertion Script.sql",
+    "AA02RJ57 4 Optimized Data Retrieval & Querying.sql",
+    "AA02RJ57 5 Database Normalization & Constraints Enforcement.sql",
+    "AA02RJ57 6 Comprehensive CRUD Operations.sql",
+    "AA02RJ57 7 Database Schema Modification & Optimization.sql",
+    "AA02RJ57 8 Stress Testing Script.sql",
+]
